a2939ea2d405b4dd110f199d61230f34bf964256
Andre Baltieri <andrebaltieri@hotmail.com>
2019-06-19 00:24:24 -0300

    Adicionado animacoes

--- lib/widgets/search-box.widget.dart
+++ lib/widgets/search-box.widget.dart
@@ -1,44 +1,155 @@
 import 'package:flutter/material.dart';
 
-class SearchBox extends StatelessWidget {
+class SearchBox extends StatefulWidget {
+  @override
+  _SearchBoxState createState() => _SearchBoxState();
+}
+
+class _SearchBoxState extends State<SearchBox>
+    with SingleTickerProviderStateMixin {
+  bool menuOpened = false;
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new AnimationController(
+      vsync: this,
+      duration: Duration(
+        seconds: 1,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        InputAnimation(
+          controller: _controller,
+        ),
+        FlatButton(
+          onPressed: () {
+            !menuOpened ? _controller.forward() : _controller.reverse();
+            menuOpened = !menuOpened;
+          },
+          child: AnimatedIcon(
+            icon: AnimatedIcons.menu_close,
+            progress: _controller,
+            semanticLabel: 'More',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class InputAnimation extends StatelessWidget {
+  final AnimationController controller;
+  final Animation<Color> containerColorAnim;
+  final Animation<double> containerOpacityAnim;
+  final Animation<double> containerBorderRadiusAnim;
+  final Animation<double> containerSizeAnim;
+
+  InputAnimation({this.controller})
+      : containerOpacityAnim = Tween(
+          begin: 0.0,
+          end: 1.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.7, 0.9),
+          ),
+        ),
+        containerSizeAnim = Tween(
+          begin: 60.0,
+          end: 280.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.2, 0.3),
+          ),
+        ),
+        containerBorderRadiusAnim = Tween(
+          begin: 0.0,
+          end: 30.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.25, 0.4),
+          ),
+        ),
+        containerColorAnim = ColorTween(
+          begin: Colors.white.withOpacity(0),
+          end: Colors.black12,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.0, 0.25),
+          ),
+        );
+
+  Widget _animate(context, child) {
     return Container(
       height: 60,
-      padding: EdgeInsets.only(
-        left: 20,
-      ),
       decoration: BoxDecoration(
-        color: Color.fromRGBO(0, 0, 0, 0.1),
+        color: containerColorAnim.value,
         borderRadius: BorderRadius.all(
-          Radius.circular(128),
+          Radius.circular(
+            containerBorderRadiusAnim.value,
+          ),
         ),
       ),
-      child: Row(
-        children: <Widget>[
-          Icon(Icons.search),
-          Container(
-            width: 300,
-            padding: EdgeInsets.only(left: 10),
-            child: TextFormField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                labelText: "Search...",
-                labelStyle: TextStyle(
+      width: containerSizeAnim.value,
+      margin: EdgeInsets.all(5),
+      child: Opacity(
+        opacity: containerOpacityAnim.value,
+        child: Row(
+          children: <Widget>[
+            SizedBox(
+              width: 20,
+            ),
+            Icon(Icons.search),
+            SizedBox(
+              width: 20,
+            ),
+            Container(
+              width: 200,
+              child: TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  labelText: "Search...",
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 16,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 20,
                   color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.w300,
-                  fontSize: 16,
                 ),
               ),
-              style: TextStyle(
-                fontSize: 20,
-                color: Theme.of(context).primaryColor,
-              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: _animate,
+      animation: controller,
     );
   }
 }
